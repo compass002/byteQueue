4d11f4219101a6d7ff859fac986e5745b92b742b
Xinlei Cao <lukecxl@gmail.com>
2013-04-17 14:07:16 -0400

    designing

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -4,19 +4,15 @@ struct Node
 	unsigned int queueID;
 	unsigned int next;
 };
-
 struct Q
 {
-	static unsigned int queueID;
-	static Node indexArray[2048];
-
 	unsigned int begin;
 	unsigned int end;
     int size;
 };
-unsigned int Q::queueID = 0;// set 0 as default, means on queue created
-Node Q::indexArray[2048] = {};
 
+unsigned int numberOfQueues;
+Node indexArray[2048];
 unsigned char data[2048];// the given memory
 // Creates a FIFO byte queue, returning a handle to it. 
 Q * create_queue()
