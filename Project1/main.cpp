ff476d69cc98b7494faf4d29d4c09fa1dd0c18b7
Xinlei Cao <lukecxl@gmail.com>
2013-04-17 16:05:58 -0400

    coding

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -1,35 +1,51 @@
-struct Q
+#include <iostream>
+using namespace std;
+//struct Q
+//{
+//	unsigned char *begin;
+//	unsigned char *end;
+//    unsigned char *size;
+//	unsigned char *capacity;
+//	const int EXPAND = 4;
+//};
+//
+//// the given memory
+//unsigned char data[2048] = {};
+//
+//// search for availavle space
+//int getSpaceIndex()
+//{
+//	for(int i = 0; i != 2048; i ++)
+//	{
+//		if
+//	}
+//}
+//
+//// Creates a FIFO byte queue, returning a handle to it. 
+//Q * create_queue()
+//{
+//	
+//} 
+//// Destroy an earlier created byte queue. 
+//void destroy_queue(Q * q)
+//{
+//
+//}
+//// Adds a new byte to a queue. 
+//void enqueue_byte(Q * q, unsigned char b)
+//{
+//
+//}
+//// Pops the next byte off the FIFO queue 
+//unsigned char dequeue_byte(Q * q); 
+
+
+int main()
 {
-	unsigned char *begin;
-	unsigned char *end;
-    unsigned char *size;
-	unsigned char *capacity;
-	const int EXPAND = 4;
-};
-
-// the given memory
-unsigned char data[2048];
-
-// search for availavle space
-int getSpaceIndex()
-{
-
+	unsigned char data[2] = {'\0'};
+	unsigned char data2;
+	cout<<data[0]<<"   "<<data[1]<<endl;
+	cout<<data2;
+	system("pause");
+	return 0;
 }
-
-// Creates a FIFO byte queue, returning a handle to it. 
-Q * create_queue()
-{
-	
-} 
-// Destroy an earlier created byte queue. 
-void destroy_queue(Q * q)
-{
-
-}
-// Adds a new byte to a queue. 
-void enqueue_byte(Q * q, unsigned char b)
-{
-
-}
-// Pops the next byte off the FIFO queue 
-unsigned char dequeue_byte(Q * q); 
