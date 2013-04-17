5042393d404ba256e1fb6c7c4af838a2bfe2b752
Xinlei Cao <lukecxl@gmail.com>
2013-04-17 16:40:11 -0400

    coding

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -1,51 +1,64 @@
 #include <iostream>
 using namespace std;
-//struct Q
-//{
-//	unsigned char *begin;
-//	unsigned char *end;
-//    unsigned char *size;
-//	unsigned char *capacity;
-//	const int EXPAND = 4;
-//};
-//
-//// the given memory
-//unsigned char data[2048] = {};
-//
-//// search for availavle space
-//int getSpaceIndex()
-//{
-//	for(int i = 0; i != 2048; i ++)
-//	{
-//		if
-//	}
-//}
-//
-//// Creates a FIFO byte queue, returning a handle to it. 
-//Q * create_queue()
-//{
-//	
-//} 
-//// Destroy an earlier created byte queue. 
-//void destroy_queue(Q * q)
-//{
-//
-//}
-//// Adds a new byte to a queue. 
-//void enqueue_byte(Q * q, unsigned char b)
-//{
-//
-//}
-//// Pops the next byte off the FIFO queue 
-//unsigned char dequeue_byte(Q * q); 
+
+//head
+//tail
+//size
+//expand = 32
+//next_break
+//data
+typedef unsigned char * Q;
+
+// the given memory
+unsigned char data[2048] = {'\0'};
+
+// search for availavle space
+int get_memory_index()
+{
+	bool continuous = false;
+	for(int i = 0; i != 2048;)
+	{
+		if(data[i] == '\0')
+		{
+			continuous = true;
+			if(i + 32 != 2048)
+			{
+				for( int j = 1; j != 32; j ++ )
+				{
+					if(data[i+j] != '\0')
+					{
+						continuous = false;
+						i = i+j;
+						break;
+					}
+				}
+			}
+		}
+	}
+}
+
+// Creates a FIFO byte queue, returning a handle to it. 
+Q * create_queue()
+{
+	
+} 
+// Destroy an earlier created byte queue. 
+void destroy_queue(Q * q)
+{
+
+}
+// Adds a new byte to a queue. 
+void enqueue_byte(Q * q, unsigned char b)
+{
+
+}
+// Pops the next byte off the FIFO queue 
+unsigned char dequeue_byte(Q * q); 
 
 
 int main()
 {
-	unsigned char data[2] = {'\0'};
-	unsigned char data2;
-	cout<<data[0]<<"   "<<data[1]<<endl;
-	cout<<data2;
+	
 	system("pause");
 	return 0;
 }
