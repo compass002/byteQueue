5575e78a99fdd43ea6aaf372d6f6c2e59d884470
Xinlei Cao <lukecxl@gmail.com>
2013-04-17 18:24:53 -0400

    coding

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -4,6 +4,7 @@ using namespace std;
 //head_index
 //tail_index
 //size
+//blocks
 //(expand = 32)
 //next_block_index
 //datas
@@ -21,9 +22,9 @@ int get_memory_index()
 		if(data[i] == '\0')
 		{
 			bool continuous = true;
-			if(i + (32+4) < 2048)
+			if(i + 32 < 2048)
 			{
-				for( int j = 1; j != (32+4); j ++ )
+				for( int j = 1; j != 32; j ++ )
 				{// check if there is 32 continuous space
 					if(data[i+j] != '\0')
 					{// not continuous
@@ -53,12 +54,13 @@ Q * create_queue()
 		data[index] = '0' + index;//head_index
 		data[index + 1] = '0' + index;//tail_index
 		data[index + 2] = '0';//size
-		data[index + 3] = '\0';//next_block_index
+		data[index + 3] = 1;//blocks
+		data[index + 4] = '\0';//next_block_index
 		//datas
-		for(int i = 0; i != 32; i ++)
+		for(int i = 0; i != 32-5; i ++)
 		{
 			// mark the space
-			data[index + 4 + i] = '.';
+			data[index + 5 + i] = '.';
 		}
 		unsigned char *pHead = &data[index];
 		return pHead;
@@ -83,13 +85,13 @@ void destroy_queue(Q * q)
 	if(q != 0)
 	{
 		int head_index = *q - '0';
-		int next = data[head_index + 3];
-		clear(head_index, head_index+4+32);
+		int next = data[head_index + 4];
+		clear(head_index, head_index+32);
 		while(next != '\0')
 		{
 			int temp_index = next;
-			next = data[temp_index+3];
-			clear(temp_index, temp_index+4+32);
+			next = data[temp_index+4];
+			clear(temp_index, temp_index+32);
 		}
 	}else
 	{
@@ -99,10 +101,36 @@ void destroy_queue(Q * q)
 // Adds a new byte to a queue. 
 void enqueue_byte(Q * q, unsigned char b)
 {
+	if(q != 0)
+	{
+		int head_index = *q - '0';
+		int blocks = data[head_index + 3];
+		int capacity = blocks * 32;
+		int size = data[head_index + 2];
+		int free_space = capacity - size;
+		if( free_space >= 0 )
+		{// space is enough
 
+		}else
+		{
+			int new_index = get_memory_index();
+			if( new_index != -1 )
+			{
+
+			}else
+			{// fail
+			}
+		}
+
+	}else
+	{
+		// error
+	}
 }
 // Pops the next byte off the FIFO queue 
-unsigned char dequeue_byte(Q * q); 
+unsigned char dequeue_byte(Q * q)
+{
+}
 
 
 int main()
